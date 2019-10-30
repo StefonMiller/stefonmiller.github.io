0a719b49e3c143aa7b70415c01057bfabd77c56d
StefonMiller <35376880+StefonMiller@users.noreply.github.com>
2019-10-30 18:04:10 -0400

    Add files via upload
    Removed inline JS and added link functionality to projects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-function show()
+function showElements(id)
 {
-    document.write("Clicked");
+    if(event.target.tagName.toLowerCase() ==='li')
+    {
+        if(id.innerHTML.charAt(0) == '+')
+        {
+            txt = id.innerHTML.split("");
+            txt[0] = '-';
+            id.innerHTML=txt.join("");
+            c = id.children[0].children;
+            for(i=0; i < c.length; i++)
+            {
+                c[i].style.display = "block";
+            }
+                
+        }
+        else
+        {
+            txt = id.innerHTML.split("");
+            txt[0] = '+';
+            id.innerHTML=txt.join("");
+            c = id.children[0].children;
+            for(i=0; i < c.length; i++)
+            {
+                c[i].style.display = "none";
+            }
+        }
+    }
+    else if(event.target.tagName.toLowerCase() ==='a')
+    {
+        event.stopPropagation();
+    }
+    
+}
+
+function recolor(id)
+{
+    
 }
